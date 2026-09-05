ca9051245a0884d5ac2332ef030c78a12ad4b01b
PHAT2417 <110274715+PHAT2417@users.noreply.github.com>
2026-09-05 10:16:27 -0700

    Dividing Space for Collaborators

---
+++ Lab_1_Assignment.py
@@ -0,0 +1,14 @@
+# Lab 1 Assignment
+# Group 7 - Xuan Phat Tran, Kristine Nguyen, Fernanda Lugo
+# 09/06/2026
+# Program description:
+
+#Fernanda Lugo's part:
+
+
+
+#Kristine Nguyen's part:
+
+
+
+#Xuan Phat Tran's part:
